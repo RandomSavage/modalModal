35ff6ceae102190715420badb5474b2e30d75571
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2021-08-23 20:37:01 -0400

    image experiments

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let x = document.getElementById('side-img')
-
+let z = document.getElementById('side-img1')
 var i = 0;
 setInterval(function() {
 x.setAttribute('style', `filter: hue-rotate(${i}deg)`);
+z.setAttribute('style', `filter: hue-rotate(${i}deg)`);
 
 if(i < 360) {
    i++ 
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,9 +1,11 @@
 "use strict";
 
 var x = document.getElementById('side-img');
+var z = document.getElementById('side-img1');
 var i = 0;
 setInterval(function () {
   x.setAttribute('style', "filter: hue-rotate(".concat(i, "deg)"));
+  z.setAttribute('style', "filter: hue-rotate(".concat(i, "deg)"));
 
   if (i < 360) {
     i++;
