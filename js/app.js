f9e09b8ffb6ba9d87737a4e7fd29eae7455c9635
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2021-08-24 21:55:57 -0400

    form pt1

--- js/app.js
+++ js/app.js
@@ -1,37 +1,81 @@
 // console.log(window.document)
-let funkyReg = '/\S+@\S+\.\S+/'
+var funkyReg = '/\S+@\S+\.\S+/'
 
-/* window.onload = function() {
+console.log(navigator)
+window.onload = function() {
+   let emailState = false
+
    let emailModal = document.getElementsByClassName('email-modal')[0]
-   console.log(emailModal)
+   // console.log(emailModal)
+
+   let emailForm = document.getElementsByClassName('email-modal__form-group')[0]
+
+   let emailError = document.getElementsByClassName('email-modal__error-message')[0]
+
+   let emailInput = document.getElementsByClassName('email-modal__input')[0]
+
+   let emailButton = document.getElementsByClassName('email-modal__button')[0]
+
+   function emailIsValid(email) {
+      return /\S+@\S+\.\S+/.test(email)
+   }
+
+   emailButton.addEventListener('click', () => {
+      if(emailIsValid(emailInput.value)) {
+      console.log(emailInput.value)
+      emailError.classList.remove('email-modal__error-message--active')
+      emailForm.classList.remove('email-modal__form-group--error')
+      }
+      else{
+         emailError.classList.toggle('email-modal__error-message--active')
+         emailForm.classList.toggle('email-modal__form-group--error')
+         // emailError.innerText = "Nope, this is not a valid email"
+         // alert('email not valid')
+      }
+      // console.log(emailInput.value)
+   })
+
+   let showModal = () => {
+      if(emailState === false) {
+         emailModal.classList.add('email-modal--visible')
+         console.log('mouse left')
+         emailState = true
+      }
+   }
 
    let closeModal = document.getElementsByClassName('email-modal__close-btn')[0]
 
    closeModal.addEventListener('click', () => {
       emailModal.classList.remove('email-modal--visible')
+      clearInterval(lizardEfx)
    })
+   
+      document.body.addEventListener('mouseleave', () => {
+         showModal()
+      })
+   }
 
-   document.addEventListener('mouseleave', () => {
-      emailModal.classList.add('email-modal--visible')
-      console.log('mouse left')
-   })
-} */
+
 
 
 let x = document.getElementById('side-img')
 let z = document.getElementById('side-img1')
 var i = 0;
-setInterval(function() {
+let lizardEfx = setInterval(function() {
 x.setAttribute('style', `filter: hue-rotate(${i}deg)`);
 z.setAttribute('style', `filter: hue-rotate(${i}deg)`);
 
 if(i < 360) {
    i++ 
-   deg.innerText = i 
+   deg.innerText = i
+   // used this to debug the clear interval-> 
+   // console.log(i)
   } else {
      i = 0 
     }
 }, 50)
+
+
 
 var deg = document.getElementById('deg')
 
--- js/dist/app.dev.js
+++ js/dist/app.dev.js
@@ -2,32 +2,63 @@
 
 // console.log(window.document)
 var funkyReg = '/\S+@\S+\.\S+/';
-/* window.onload = function() {
-   let emailModal = document.getElementsByClassName('email-modal')[0]
-   console.log(emailModal)
+console.log(navigator);
 
-   let closeModal = document.getElementsByClassName('email-modal__close-btn')[0]
+window.onload = function () {
+  var emailState = false;
+  var emailModal = document.getElementsByClassName('email-modal')[0]; // console.log(emailModal)
 
-   closeModal.addEventListener('click', () => {
-      emailModal.classList.remove('email-modal--visible')
-   })
+  var emailForm = document.getElementsByClassName('email-modal__form-group')[0];
+  var emailError = document.getElementsByClassName('email-modal__error-message')[0];
+  var emailInput = document.getElementsByClassName('email-modal__input')[0];
+  var emailButton = document.getElementsByClassName('email-modal__button')[0];
 
-   document.addEventListener('mouseleave', () => {
-      emailModal.classList.add('email-modal--visible')
-      console.log('mouse left')
-   })
-} */
+  function emailIsValid(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
+  emailButton.addEventListener('click', function () {
+    if (emailIsValid(emailInput.value)) {
+      console.log(emailInput.value);
+      emailError.classList.remove('email-modal__error-message--active');
+      emailForm.classList.remove('email-modal__form-group--error');
+    } else {
+      emailError.classList.toggle('email-modal__error-message--active');
+      emailForm.classList.toggle('email-modal__form-group--error'); // emailError.innerText = "Nope, this is not a valid email"
+      // alert('email not valid')
+    } // console.log(emailInput.value)
+
+  });
+
+  var showModal = function showModal() {
+    if (emailState === false) {
+      emailModal.classList.add('email-modal--visible');
+      console.log('mouse left');
+      emailState = true;
+    }
+  };
+
+  var closeModal = document.getElementsByClassName('email-modal__close-btn')[0];
+  closeModal.addEventListener('click', function () {
+    emailModal.classList.remove('email-modal--visible');
+    clearInterval(lizardEfx);
+  });
+  document.body.addEventListener('mouseleave', function () {
+    showModal();
+  });
+};
 
 var x = document.getElementById('side-img');
 var z = document.getElementById('side-img1');
 var i = 0;
-setInterval(function () {
+var lizardEfx = setInterval(function () {
   x.setAttribute('style', "filter: hue-rotate(".concat(i, "deg)"));
   z.setAttribute('style', "filter: hue-rotate(".concat(i, "deg)"));
 
   if (i < 360) {
     i++;
-    deg.innerText = i;
+    deg.innerText = i; // used this to debug the clear interval-> 
+    // console.log(i)
   } else {
     i = 0;
   }
